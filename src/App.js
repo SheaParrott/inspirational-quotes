6806608b73b3663a153c69abfe5f2a1b29b8a2e9
SheaParrott <alexandergotthis@gmail.com>
2018-12-07 13:55:38 -0500

    background color change, text-styling & @media added

--- src/App.js
+++ src/App.js
@@ -4,35 +4,64 @@ import './App.css'
 import axios from 'axios'
 
 class App extends Component {
+  CORS_ANYWHERE_URL = 'https://cors-anywhere.herokuapp.com/'
+
   constructor(props) {
     super(props)
 
     this.state = {
-      quote: ''
+      quote: '',
+      author: '',
+      image: ''
     }
   }
-  newQuote = () => {
+
+  componentDidMount = () => {
     axios
       .get(
-        'http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en'
+        'https://api.unsplash.com/photos/random?count=1&client_id=a99ca210f159a57ffe3e1ee488fc1d1930614d06093b49671699a5319fc88309'
       )
-      .then(Response => {
-        console.log(Response.data)
-        console.log('clicked')
+      .then(response => {
+        console.log(response.data[0].urls.regular)
+        this.setState({
+          image: response.data[0].urls.regular
+        })
       })
+    axios
+      .get(
+        `${
+          this.CORS_ANYWHERE_URL
+        }http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en`
+      )
+      .then(response => {
+        this.setState({
+          quote: response.data.quoteText,
+          author: response.data.quoteAuthor,
+          link: response.data.quoteLink
+        })
+      })
+  }
+  insertQuote = () => {
+    if (this.state.quote) {
+      return (
+        <div className="quote">
+          <p>{this.state.quote}</p>
+          <h6>-{this.state.author}</h6>
+        </div>
+      )
+    }
   }
   render() {
     return (
       <div className="App">
-        <h1>Inspirational Quotes</h1>
-        <button onClick={this.newQuote}>Get New Quote</button>
-        <p>
-          Through meditation and by giving full attention to one thing at a
-          time, we can learn to direct attention where we choose.
-        </p>
-        <a href="http://forismatic.com/en/83102bee09/">
-          <p>-Eknath Easwaran</p>
-        </a>
+        <header>
+          <h1>Inspirational Quotes</h1>
+          <h3 onClick={this.componentDidMount}>Get New Quote</h3>
+        </header>
+        <main>
+          <img src={this.state.image} alt="UnSplash" />
+          {this.insertQuote()}
+        </main>
       </div>
     )
   }
